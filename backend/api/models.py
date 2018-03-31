c69500acab205bab04ec0ec96f1309af9724e1c0
John Kwening <kwening.john@gmail.com>
2018-03-31 15:20:42 -0400

    feat: Add dictionary model and table to db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Dictionary(models.Model):
+  word = models.CharField(max_length=50, primary_key=True)
+  definition = models.TextField()
+  part_of_speech = models.CharField(max_length=50)
+
+  class Meta:
+    db_table = 'dictionary' # override database table name
